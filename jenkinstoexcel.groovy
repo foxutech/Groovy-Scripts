22ec735b5ee53fb075d557bd0dc2b5ff83a265bb
FoxuTech <prabulugar@gmail.com>
2018-12-14 10:00:14 +0530

    Update jenkinstoexcel.groovy

--- jenkinstoexcel.groovy
+++ jenkinstoexcel.groovy
@@ -7,51 +7,22 @@ import jxl.write.*
 import jxl.write.Label
 import hudson.model.*
 
-log.info("Testing Started")
+log.info("Started")
 def reqOperationName = "Operation Name"
 def inputDataFileName = "FileLocation/filename.xls"
 def inputDataSheetName = "Datasheet name"
 Workbook workbook = Workbook.getWorkbook(new File(inputDataFileName))
 Sheet  sheet1 = workbook.getSheet(inputDataSheetName)
 
+def State          = params.State
+def zip            = params.zip
+def Age            = params.Age
+def Effective_Date = params.Effective_Date
+def Plan_Code      = params.Plan_Code
 
 def myList = new ArrayList<String>();
 def groovyUtils = new com.eviware.soapui.support.GroovyUtils(context)
-String xmlResponse = reqOperationName+"#Request"
-def reqholder = groovyUtils.getXmlHolder(xmlResponse)
-try{
-    rowcount = sheet1.getRows()
-    colcount = sheet1.getColumns()
 
-    for(Row in 1..rowcount-1){
-
-    String reqTagName = sheet1.getCell(0,0).getContents()
-
-    def TagCount = reqholder["count(//*:"+reqTagName+")"]
-
-    if(TagCount!=0){
-        String reqTagValue = sheet1.getCell(0,Row).getContents()
-        if(reqTagValue!=null && !reqTagValue.isEmpty() && reqTagValue!="")
-                {
-                    reqholder = groovyUtils.getXmlHolder(xmlResponse)
-                    log.info "extracted value : " + reqTagValue
-                reqholder.setNodeValue("//*:"+reqTagName, reqTagValue)
-                reqholder.updateProperty()        
-                log.info "node value : " + reqholder.getNodeValue("//*:"+reqTagName)
-                //test the request
-                testRunner.runTestStepByName(reqOperationName)
-                reqholder = groovyUtils.getXmlHolder(reqOperationName+"#Response")
-                myList.add(reqholder.getPrettyXml().toString())
-                log.info myList[Row-1]
-                }                      
-    }
-
-    }
-}
-catch (Exception e) {log.info(e)}
-finally{
-    workbook.close()
-}
 Workbook existingWorkbook = Workbook.getWorkbook(new File(inputDataFileName));
 WritableWorkbook workbookCopy = Workbook.createWorkbook(new File(inputDataFileName), existingWorkbook);
 
@@ -63,26 +34,30 @@ try
     {
     def sheetdata = System.getenv(myList[i])
 
-    resTagValue1= sheetdata.getNodeValue("//*:Response Field Element1")
+    resTagValue1= sheetdata.getNodeValue("State")
     Label l = new Label(2, i+1, resTagValue1.toString());
     cell = (WritableCell) l;
     sheetToEdit.addCell(cell);
 
-    resTagValue2= sheetdata.getNodeValue("//*:Response Field Element2")
+    resTagValue2= sheetdata.getNodeValue("zip")
     Label m = new Label(3, i+1, resTagValue2.toString());
     cell = (WritableCell) m;
     sheetToEdit.addCell(cell);
 
-    resTagValue3= sheetdata.getNodeValue("//*:Response Field Element3")
+    resTagValue3= sheetdata.getNodeValue("Age")
     Label n = new Label(4, i+1, resTagValue3.toString());
     cell = (WritableCell) n;
     sheetToEdit.addCell(cell);
 
-    resTagValue4= sheetdata.getNodeValue("//*:Response Field Element4")
+    resTagValue4= sheetdata.getNodeValue("Effective_Date")
     Label o = new Label(5, i+1, resTagValue4.toString());
     cell = (WritableCell) o;
     sheetToEdit.addCell(cell);
-
+	
+	resTagValue5= sheetdata.getNodeValue("Plan_Code")
+    Label o = new Label(6, i+1, resTagValue4.toString());
+    cell = (WritableCell) o;
+    sheetToEdit.addCell(cell);
     }
 }
 catch (Exception e) {log.info(e)}
@@ -91,4 +66,4 @@ finally{
  workbookCopy.close();
  existingWorkbook.close();
 }
-log.info("Testing Over")
+log.info("Completed")
